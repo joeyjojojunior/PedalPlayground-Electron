a5e1eb3d903e38739a13ff35276eac15b4f5e1da
joeyjojojunior <cmdrjoeyjojojunior@gmail.com>
2022-02-13 03:41:13 -0500

    disable drag and drop file into window & ctrl+r to reload pagE

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 const { dialog } = require('electron');
 const electron = require('electron');
 const ipcMain = electron.ipcMain;
 const path = require('path');
 const fs = require('fs');
-
-
 
 // Enable live reload for Electron too
 require('electron-reload')(__dirname, 
@@ -37,10 +35,10 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://127.0.0.1:8080/index.html');
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
- 
   mainWindow.webContents.on('did-finish-load', () => {
+
     ipcMain.on('save-preset', (event, preset, presetName) => {
       fs.mkdir(presetDir, e => {
         if (e && e.code === 'EEXIST') {
@@ -112,7 +110,24 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
+
+
 })
+
+app.on('browser-window-blur', function () {
+  globalShortcut.unregister('CommandOrControl+R');
+  globalShortcut.unregister('F5');
+});
+
+app.on('browser-window-focus', function () {
+  globalShortcut.register("CommandOrControl+R", () => {
+      console.log("CommandOrControl+R is pressed: Shortcut Disabled");
+  });
+  globalShortcut.register("F5", () => {
+      console.log("F5 is pressed: Shortcut Disabled");
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
